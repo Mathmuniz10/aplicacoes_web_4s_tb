1084f75b9d60dfee07ecc9c5ce7b4096bb8728a5
Mathmuniz10 <matheusmarcosmuniz63@gmail.com>
2026-09-24 22:14:49 -0300

    estado atual do projeto com a pesquisa de imagens implementada

---
+++ src/main/java/com/example/imagemPecas/application/images/imagesController.java
@@ -0,0 +1,91 @@
+package com.example.imagemPecas.application.images;
+
+import com.example.imagemPecas.domain.entity.Image;
+import com.example.imagemPecas.domain.enums.ImageExtension;
+import com.example.imagemPecas.domain.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/v1/images")
+@Slf4j
+@RequiredArgsConstructor
+public class imagesController {
+
+    private final ImageService service;
+    private final ImageMapper mapper;
+
+    @PostMapping
+    public ResponseEntity save(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("tags") List<String> tags
+    ) throws IOException
+    {
+        log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
+;
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage =  service.save(image);
+
+        URI imageUri = buildImageURL(savedImage);
+        return ResponseEntity.created(imageUri).build();
+    }
+
+    // localhost:8080/v1/images?extension=PNG&query=Nature
+    @GetMapping
+    public ResponseEntity<List<ImageDTO>> search(
+            @RequestParam(value = "extension", required = false, defaultValue = "") String extension,
+            @RequestParam(value = "query", required = false) String query
+    ){
+        var result = service.search(ImageExtension.ofName(extension), query);
+        var images = result.stream().map(image -> {
+            var url = buildImageURL(image);
+            return mapper.imageToDTO(image, url.toString());
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.ok(images);
+    }
+
+    // v1/images/{id}
+    @GetMapping("{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") String id){
+        var possibleImage = service.getById(id);
+        if(possibleImage.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        var image = possibleImage.get();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(image.getExtension().getMediaType());
+        headers.setContentLength(image.getSize());
+        //headers.setContentDispositionFormData("", image.getFileName());
+        headers.setContentDispositionFormData("inline; filename= \""
+                + image.getName()
+                + "\"", image.getFileName());
+
+        return new ResponseEntity<>(image.getFile(), headers, HttpStatus.OK);
+    }
+
+
+    //localhost:8080/v1/images/xxxxxxxxxxxxxxxxx
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path(imagePath)
+                .build()
+                .toUri();
+    }
+
+}
---
+++ src/main/java/com/example/imagemPecas/infra/repository/ImageRepository.java
@@ -0,0 +1,45 @@
+package com.example.imagemPecas.infra.repository;
+
+import com.example.imagemPecas.domain.entity.Image;
+import com.example.imagemPecas.domain.enums.ImageExtension;
+import com.example.imagemPecas.infra.repository.specs.GenericSpecs;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static com.example.imagemPecas.infra.repository.specs.ImageSpecs.*;
+import static org.springframework.data.jpa.domain.Specification.anyOf;
+import static org.springframework.data.jpa.domain.Specification.where;
+
+public interface ImageRepository extends JpaRepository<Image,String>, JpaSpecificationExecutor<Image> {
+
+    /**
+     *
+     * @param extension
+     * @param query
+     * @return
+     * SELECT * FROM IMAGE WHERE 1 = 1 AND EXTENSION = 'PNG' AND ( NAME LIKE 'QUERY'
+     * OR TAGS LIKE 'QUERY' )
+     */
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+        //SELECT * FROM IMAGE 1 = 1
+        Specification<Image> spec = where(GenericSpecs.conjunction());
+
+        if (extension != null){
+           //AND EXTENSION = 'PNG'
+           spec = spec.and(extensionEqual(extension));
+        }
+
+        if(StringUtils.hasText(query)){
+            //AND ( NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY' )
+            spec = spec.and( anyOf(nameLike(query), tagsLike(query)));
+        }
+
+        return findAll(spec);
+    }
+
+}
